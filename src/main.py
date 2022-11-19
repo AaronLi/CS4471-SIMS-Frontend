c964ef4575428a409d61189333e57d2d25253f85
AaronLi <dumfing@gmail.com>
2022-11-19 15:51:54 -0500

    Add skeleton service

--- src/main.py
+++ src/main.py
@@ -1,2 +1,13 @@
+from proto.frontend.frontend_pb2_grpc import SimsFrontendServicer
+
+class FrontendServicer(SimsFrontendServicer):
+
+    def CredAuth(self, request, context):
+        return super().CredAuth(request, context)
+
+    def ClientCmd(self, request, context):
+        return super().ClientCmd(request, context)
+
+
 if __name__ == '__main__':
     print("Hello, World!")
