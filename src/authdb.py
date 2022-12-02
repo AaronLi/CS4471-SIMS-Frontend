e53310e51ad08ca1aa2decc46307ad88e4b1cc2a
Erin Vu <kvu9@uwo.ca>
2022-12-01 19:42:01 -0500

    credential added

---
+++ src/authdb.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+class CredentialDB:
+    def __init__(self):
+        self.conn = sqlite3.connect('auth.db')
+        self.conn.commit()
+        self.conn.execute("CREATE TABLE IF NOT EXISTS (username TEXT PRIMARY KEY, hashedPw BLOB NOT NULL UNIQUE, token TEXT")
+        self.cur = self.conn.cursor()
+
+    def query(self, arg):
+        self.cur.execute(arg)
+        self.conn.commit()
+        return self.cur
+
+    def __del__(self):
+        self.conn.close()
+            
+
+        
--- src/main.py
+++ src/main.py
@@ -3,10 +3,45 @@ from concurrent import futures
 import frontend_proto.frontend_pb2_grpc as frontend_grpc
 import frontend_proto.frontend_messages_pb2 as frontend_messages
 import grpc
+import sqlite3
+import logging
+import secrets
+from base64 import b64encode
+from Crypto.Hash import SHA256
+from Crypto.Protocol.KDF import bcrypt, bcrypt_check
+from authdb import CredentialDB
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
 class FrontendServicer(frontend_grpc.SimsFrontendServicer):
     def CredAuth(self, request, context):
         print("CredAuth", request.username, request.password)
-        return frontend_messages.Token(token="placeholder")
+
+        try:
+            connect = CredentialDB()
+            cur = connect.query(("UPDATE credential SET token = :token WHERE username = :username",{"token":token, "username":request.username}))
+            hashedPw = cur.fetchone()
+            
+            try:
+                b64input = b64encode(SHA256.new(request.password).digest)
+                bcrypt_check(b64input, hashedPw)
+                token = secrets.token_urlsafe(16)
+                cur.execute("UPDATE credential SET token = :token WHERE username = :username",{"token":token, "username":request.username})
+                return frontend_messages.Token(token=token)
+            
+            except ValueError:
+                logger.info("Incorrect password")
+                context.set_code(grpc.StatusCode.UNAUTHENTICATED)
+                context.set_details('Incorrect password!')
+                return frontend_grpc.Response()
+            
+        except sqlite3.Error as e:
+            logger.info("Can't connect to db, error %s" % e)
+            context.set_code(grpc.StatusCode.INTERNAL)
+            context.set_details("Can't fetch from db")
+
+        # return frontend_messages.Token(token="placeholder")
 
     def ClientCmd(self, request, context):
         print("ClientCmd")
