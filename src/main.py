bfa009354b49ddfe81a1cc7fff851e5fa60e89a1
Erin Vu <kvu9@uwo.ca>
2022-12-01 21:51:45 -0500

    fix fetch hashed pw from db

--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@ class FrontendServicer(frontend_grpc.SimsFrontendServicer):
 
         try:
             connect = CredentialDB()
-            cur = connect.query(("UPDATE credential SET token = :token WHERE username = :username",{"token":token, "username":request.username}))
+            cur = connect.query(("SELECT username WHERE username = :username",{"username":request.username}))
             hashedPw = cur.fetchone()
             
             try:
