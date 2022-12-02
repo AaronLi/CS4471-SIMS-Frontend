7a82286d7f15f1f95f267d07ed9c60bf7abb7671
Erin Vu <kvu9@uwo.ca>
2022-12-02 01:03:03 -0500

    pls help generating protobuf for me

--- generate_frontend_protos.py
+++ generate_frontend_protos.py
@@ -1,11 +1,15 @@
 import os
+import sys
 import subprocess
 from importlib import util as iutil
 
 grpc_tools_id = 'grpc_tools'
 
-input_dir = os.path.join(os.getcwd(), 'proto', 'frontend_proto')
-output_dir = os.path.join(os.getcwd(), 'src')
+# sys.path.append(os.path.dirname(__file__))
+# print(sys.path)
+
+input_dir = os.path.join(os.path.dirname(__file__), 'proto', 'frontend_proto')
+output_dir = os.path.join(os.path.dirname(__file__), 'src')
 
 if iutil.find_spec(grpc_tools_id) is None:
     print("grpcio-tools not installed")
@@ -18,5 +22,5 @@ if iutil.find_spec(grpc_tools_id) is None:
 
 print(f'generating grpc files from {input_dir}')
 os.makedirs(output_dir, exist_ok=True)
-subprocess.run(['python', '-m', 'grpc_tools.protoc', f'-Iproto', f'--python_out={output_dir}', f'--pyi_out={output_dir}', f'--grpc_python_out={output_dir}', os.path.join('proto', 'frontend_proto', '*.proto')], shell=True)
+subprocess.run(['python3', '-m', 'grpc_tools.protoc', f'-Iproto', f'--python_out={output_dir}', f'--pyi_out={output_dir}', f'--grpc_python_out={output_dir}', os.path.join(os.path.dirname(__file__),'proto', 'frontend_proto', '*.proto')], shell=False)
 print(f'generated grpc files in {output_dir}')
